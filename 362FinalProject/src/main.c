e31a0832f903adbcb456399adbd67c35449316b3
Mitchell James Ciupak <ciupak@ee69lnx08.ecn.purdue.edu>
2019-10-20 11:03:33 -0400

    Game Menu

--- 362FinalProject/src/main.c
+++ 362FinalProject/src/main.c
@@ -3,7 +3,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Function Declarations
+int chooseDifficulty(void);
+
 int main(void)
 {
-    /* Student code goes here */
+	/*Game Menu*/
+	int difficulty = chooseDifficulty();
+
+
+}
+
+
+int chooseDifficulty(void) {
+	//enable the clock to GPIOA
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	//Set PA0 to input mode (00)
+	GPIOA->MODER &= 0b00;
+
+	int diff = 0;
+	while(diff == 0) {
+		//Read input
+		diff = GPIOA->IDR[0];
+	}
+	return diff;
 }
