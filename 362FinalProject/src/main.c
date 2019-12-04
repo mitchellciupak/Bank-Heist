606495908f2efdb21db66b6024dcb28b7245203d
ColinCassens <42587281+ColinCassens@users.noreply.github.com>
2019-12-04 16:58:40 -0500

    Revert "Feature audio"

--- 362FinalProject/src/main.c
+++ 362FinalProject/src/main.c
@@ -4,8 +4,8 @@
 #include <stdlib.h>
 
 #include "menu.h"
+#include "keyboard.h"
 #include "pots.h"
-#include "audio.h"
 #include "lcd.h"
 
 const char * msg1;
@@ -17,35 +17,35 @@ extern int move;
 int main(void)
 {
 	/*Menu*/
-  //Init LCD Display
-  initDisplay();
-  setup_gpio();
-  setup_timer6();
-  playAudio(PIRATE);
+    //Init LCD Display
+    initDisplay();
+    setup_gpio();
+    setup_timer6();
 
-  //Display initial message
-  msg1 = "Safe Challenge!";
-  msg4 = "          (C) to continue     ";
-  msg2 = "        Main Menu:            ";
-  topDisplayStatic();
-  move = 2;
-  char c = get_char_key();
+    //Display initial message
+    msg1 = "Safe Challenge!";
+    msg4 = "          (C) to continue     ";
+    msg2 = "        Main Menu:            ";
+    topDisplayStatic();
+    move = 2;
+    char c = get_char_key();
 
-  if(c == 'C'){
-      msg1 = "SelectDifficulty:";
-      msg2 = "   Red(Hard) : Yellow(Med) : Green(Easy)";
-      msg4 = "          Pull Wire to Continue  ";
-      topDisplayStatic();
-  }
-  //Find Difficulty
-  int difficultyMode = menuStartupDifficulty();
+    if(c == 'C'){
+        msg1 = "SelectDifficulty:";
+        msg2 = "   Red(Hard) : Yellow(Med) : Green(Easy)";
+        msg4 = "          Pull Wire to Continue  ";
+        topDisplayStatic();
+    }
+    //Find Difficulty
+    int difficultyMode = menuStartupDifficulty();
 
-  //Init Timer and 7-Seg
-  //menuInit(difficultyMode);
+    //Init Timer and 7-Seg
+    //menuInit(difficultyMode);
 
 	/*Challenge 1: Keypad Challenge*/
 	//configKeyboard(); //TODO name to keypadInit();
 
 	/*Challenge 2: Pots Challenge*/
 	potsInit();
+
 }
